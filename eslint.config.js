5367efd0f5d9ce4721e2f7fae66b6ad6978eca0f
Esdras Santiago Alvarado <edsantiago@url.edu.gt>
2026-08-24 15:48:06 -0600

    refactor: configurar analisis estatico con ESLint

---
+++ eslint.config.js
@@ -0,0 +1,34 @@
+import eslint from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+
+export default [
+  { ignores: ['dist', 'coverage'] },
+  {
+    ...eslint.configs.recommended,
+    files: ['**/*.{js,jsx}'],
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: 'module',
+      parserOptions: {
+        ecmaFeatures: { jsx: true },
+      },
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+    },
+  },
+]
